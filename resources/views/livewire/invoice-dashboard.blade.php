0c6e2941717e29e9e4fe09be6030360eb376b7e3
faisal-aali <faz.ali.bhamani@gmail.com>
2025-02-20 20:14:14 +0500

    Invoice View Task: Button Color

--- resources/views/livewire/invoice-dashboard.blade.php
+++ resources/views/livewire/invoice-dashboard.blade.php
@@ -75,7 +75,7 @@
                 Export
             </button>
 
-            <button class="px-4 py-1 text-white bg-main rounded-md flex items-center gap-2 shadow-md bg">
+            <button class="px-4 py-1 text-white bg-main rounded-md flex items-center gap-2 shadow-md bg-blue-600">
                 <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
                 </svg>
